e2c481fd703bfda8dadacd7f489df2fa994da207
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2021-02-24 17:22:53 -0300

    Criando a classe com as informções do fornecedor

---
+++ src/main/java/br/com/rodrigoamora/learning_spring_colud/dto/InfoFornecedorDTO.java
@@ -0,0 +1,15 @@
+package br.com.rodrigoamora.learning_spring_colud.dto;
+
+public class InfoFornecedorDTO {
+
+	private EnderecoDTO emdereco;
+
+	public EnderecoDTO getEmdereco() {
+		return emdereco;
+	}
+
+	public void setEmdereco(EnderecoDTO emdereco) {
+		this.emdereco = emdereco;
+	}
+	
+}
