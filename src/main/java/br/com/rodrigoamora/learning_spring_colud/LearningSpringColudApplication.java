9a657e1cfd00b56c269895637166b99d853c723a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2021-02-24 15:25:18 -0300

    Estrutura do projeto

---
+++ src/main/java/br/com/rodrigoamora/learning_spring_colud/LearningSpringColudApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.learning_spring_colud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringColudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringColudApplication.class, args);
+	}
+
+}
